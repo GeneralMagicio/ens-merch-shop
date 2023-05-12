74b417b8180a85165368bc96095bfe0458aa04f7
Vitor <marthendalnunes@gmail.com>
2023-05-12 15:14:01 -0300

    feat: add web3 provider

---
+++ site/components/providers/Web3Provider.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react'
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { mainnet, goerli } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, goerli],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains,
+})
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+})
+
+interface Web3ProviderProps {
+  children: ReactNode
+}
+
+export const Web3Provider = ({ children }: Web3ProviderProps) => (
+  <WagmiConfig config={wagmiConfig}>
+    <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+  </WagmiConfig>
+)
--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,3 +1,4 @@
+import '@rainbow-me/rainbowkit/styles.css'
 import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
@@ -6,6 +7,7 @@ import { FC, ReactNode, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
+import { Web3Provider } from '@components/providers/Web3Provider'
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
 
@@ -19,11 +21,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
-      <ManagedUIContext>
-        <Layout pageProps={pageProps}>
-          <Component {...pageProps} />
-        </Layout>
-      </ManagedUIContext>
+      <Web3Provider>
+        <ManagedUIContext>
+          <Layout pageProps={pageProps}>
+            <Component {...pageProps} />
+          </Layout>
+        </ManagedUIContext>
+      </Web3Provider>
     </>
   )
 }
