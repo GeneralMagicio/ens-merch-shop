2a2d74ca5fe26bffb9469edc8fad40d9935c3f2e
Vitor <marthendalnunes@gmail.com>
2023-05-15 21:06:42 -0300

    feat: add terms and conditions page

---
+++ site/components/common/FormatMarkdown/FormatMarkdown.tsx
@@ -0,0 +1,25 @@
+import ReactMarkdown from 'react-markdown'
+import remarkBreaks from 'remark-breaks'
+import cn from 'clsx'
+
+interface FormatMarkdownProps {
+  content: string
+  size?: 'sm' | 'base' | 'lg' | 'xl' | '2xl'
+  className?: string
+}
+
+const FormatMarkdown = ({ content, className, size }: FormatMarkdownProps) => (
+  <ReactMarkdown
+    linkTarget="_blank"
+    remarkPlugins={[remarkBreaks]}
+    className={cn(
+      'prose max-w-full overflow-hidden',
+      size && `prose-${size}`,
+      className
+    )}
+  >
+    {content}
+  </ReactMarkdown>
+)
+
+export default FormatMarkdown
