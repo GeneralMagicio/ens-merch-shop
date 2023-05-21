a3b8d0c7deb9ead504f17fc472ddc35d2c2ef5ad
Vitor <marthendalnunes@gmail.com>
2023-05-20 22:14:01 -0300

    fix: avatar color

--- site/components/common/Avatar/Avatar.tsx
+++ site/components/common/Avatar/Avatar.tsx
@@ -1,12 +1,14 @@
-import { FC, useRef, useEffect } from 'react'
+import { FC, useRef } from 'react'
 import { useUserAvatar } from '@lib/hooks/useUserAvatar'
+import cn from 'clsx'
 
 interface Props {
   className?: string
   children?: any
+  variant?: 'default' | 'light'
 }
 
-const Avatar: FC<Props> = ({}) => {
+const Avatar: FC<Props> = ({ variant = 'default' }) => {
   let ref = useRef() as React.MutableRefObject<HTMLInputElement>
   let { userAvatar } = useUserAvatar()
 
@@ -14,7 +16,10 @@ const Avatar: FC<Props> = ({}) => {
     <div
       ref={ref}
       style={{ backgroundImage: userAvatar }}
-      className="inline-block h-8 w-8 rounded-full border-2 border-primary hover:border-secondary focus:border-secondary transition-colors ease-linear"
+      className={cn(
+        'inline-block h-8 w-8 rounded-full border-2 hover:scale-110 focus:border-secondary transition-all ease-linear',
+        variant === 'default' ? 'border-blue-primary' : 'border-primary'
+      )}
     >
       {/* Add an image - We're generating a gradient as placeholder  <img></img> */}
     </div>
