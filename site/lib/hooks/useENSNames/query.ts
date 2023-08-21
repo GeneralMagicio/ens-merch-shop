cb79cbc0bcc989e5a509adf26bbbe12b9fa3f573
Jorge Pérez <jorge@ktlxv.com>
2023-08-21 21:08:32 +0200

    Query ENS names both by owner and by resolved address

--- site/lib/hooks/useENSNames/query.ts
+++ site/lib/hooks/useENSNames/query.ts
@@ -2,7 +2,10 @@ import { gql } from 'graphql-request';
 
 export const queryDomains = gql`
 	query queryDomains($owner: String!) {
-		domains(first: 1000, where: { owner_: { id: $owner } }) {
+		domains(
+			first: 1000
+			where: { or: [{ owner: $owner }, { resolvedAddress: $owner }] }
+		) {
 			id
 			name
 			labelName
